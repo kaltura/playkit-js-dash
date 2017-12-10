ad7a6dc3cb81f66dcf3cbd7eb549958b4d251903
odedhutzler <31587052+odedhutzler@users.noreply.github.com>
2017-12-10 17:59:13 +0200

    feat: add default-config json container (#39)
    Add ;default-config.json; for component.
    Initial config - ignore VTT http bad response and parse issues - closes FEC-7593.

--- src/dash-adapter.js
+++ src/dash-adapter.js
@@ -6,6 +6,7 @@ import {Utils} from 'playkit-js'
 import {Error} from 'playkit-js'
 import Widevine from './drm/widevine'
 import PlayReady from './drm/playready'
+import {defaultConfig} from './default-config'
 
 /**
  * Adapter of shaka lib for dash content
@@ -174,6 +175,7 @@ export default class DashAdapter extends BaseMediaSourceAdapter {
   constructor(videoElement: HTMLVideoElement, source: Object, config: Object = {}) {
     DashAdapter._logger.debug('Creating adapter. Shaka version: ' + shaka.Player.version);
     super(videoElement, source, config);
+    this._config = Utils.Object.mergeDeep({}, this._config, defaultConfig);
   }
 
   /**
