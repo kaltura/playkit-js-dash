a087cae5e47313d4db4c19c9b90dce6a8c2d820e
Dan Ziv <dan.ziv@kaltura.com>
2018-07-16 13:04:45 +0300

    chore: prettier (#57)
    Add prettier settings for code style.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const webpack = require("webpack");
-const path = require("path");
-const PROD = (process.env.NODE_ENV === 'production');
-const packageData = require("./package.json");
+const webpack = require('webpack');
+const path = require('path');
+const PROD = process.env.NODE_ENV === 'production';
+const packageData = require('./package.json');
 
 let plugins = [
   new webpack.DefinePlugin({
@@ -17,63 +17,63 @@ if (PROD) {
 }
 
 module.exports = {
-  context: __dirname + "/src",
-  entry: {"playkit-dash": "index.js"},
+  context: __dirname + '/src',
+  entry: {'playkit-dash': 'index.js'},
   output: {
-    path: __dirname + "/dist",
+    path: __dirname + '/dist',
     filename: '[name].js',
-    library: ["playkit", "dash"],
-    libraryTarget: "umd",
-    devtoolModuleFilenameTemplate: "./dash/[resource-path]",
+    library: ['playkit', 'dash'],
+    libraryTarget: 'umd',
+    devtoolModuleFilenameTemplate: './dash/[resource-path]'
   },
   devtool: 'source-map',
   plugins: plugins,
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: [{
-        loader: "babel-loader"
-      }],
-      exclude: [
-        /node_modules/
-      ]
-    }, {
-      test: /\.js$/,
-      exclude: [
-        /node_modules/
-      ],
-      enforce: 'pre',
-      use: [{
-        loader: 'eslint-loader',
-        options: {
-          rules: {
-            semi: 0
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
           }
-        }
-      }],
-    }]
-  },
-  devServer: {
-    contentBase: __dirname + "/src"
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname, "src"),
-      "node_modules"
+        ],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              rules: {
+                semi: 0
+              }
+            }
+          }
+        ]
+      }
     ]
   },
+  devServer: {
+    contentBase: __dirname + '/src'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
   externals: {
-    "playkit-js": {
-      commonjs: "playkit-js",
-      commonjs2: "playkit-js",
-      amd: "playkit-js",
-      root: ["playkit", "core"]
+    'playkit-js': {
+      commonjs: 'playkit-js',
+      commonjs2: 'playkit-js',
+      amd: 'playkit-js',
+      root: ['playkit', 'core']
     },
-    "shaka-player": {
-      commonjs: "shaka-player",
-      commonjs2: "shaka-player",
-      amd: "shaka-player",
-      root: "shaka"
+    'shaka-player': {
+      commonjs: 'shaka-player',
+      commonjs2: 'shaka-player',
+      amd: 'shaka-player',
+      root: 'shaka'
     }
   }
 };
