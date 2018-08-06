e119925034336450425fde6a785dd260dad69902
Oren Me <oren.me@gmail.com>
2018-08-06 10:07:07 +0300

    test: add better unit test reporter (#59)
    add karma mocha reporter for better looking unit test report: https://www.npmjs.com/package/karma-mocha-reporter
    https://user-images.githubusercontent.com/5461862/43686442-641f1c5a-98ce-11e8-8ada-23e1392f2e52.png

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,10 @@ module.exports = function(config) {
       'src/**/*.js': ['webpack', 'sourcemap'],
       'test/setup/karma.js': ['webpack', 'sourcemap']
     },
-    reporters: ['progress', 'coverage'],
+    reporters: [
+      'mocha',
+      'coverage'
+    ],
     webpack: webpackConfig,
     webpackServer: {
       noInfo: true
