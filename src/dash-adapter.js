0078cfd60321851514a0cbb573e43d873aaba259
yuval.keidar <keidaryu@gmail.com>
2021-01-27 13:46:07 +0200

    fix(FEC-10773): reuse shaka load option to start load from the detached point
    Issue: attachMediaSource start to load from the beginning instead of the last detach point.
    Solution: start load from the last detach point.

--- src/dash-adapter.js
+++ src/dash-adapter.js
@@ -529,20 +529,6 @@ export default class DashAdapter extends BaseMediaSourceAdapter {
         Utils.Dom.removeAttribute(this._videoElement, 'src');
       }
       this._init();
-      const _seekAfterDetach = () => {
-        if (isNaN(this._lastTimeDetach)) return;
-        if (parseInt(this._lastTimeDetach) === parseInt(this.duration)) {
-          this.currentTime = 0;
-        } else {
-          this.currentTime = this._lastTimeDetach;
-        }
-        this._lastTimeDetach = NaN;
-      };
-      if (!isNaN(this._lastTimeDetach)) {
-        this._eventManager.listenOnce(this._videoElement, EventType.LOADED_DATA, _seekAfterDetach);
-        //change to NaN to avoid the seek after detach whenever seeked fired before - SmartTV issue
-        this._eventManager.listenOnce(this._videoElement, EventType.SEEKED, () => (this._lastTimeDetach = NaN));
-      }
     }
   }
 
@@ -647,7 +633,8 @@ export default class DashAdapter extends BaseMediaSourceAdapter {
       this._loadPromise = new Promise((resolve, reject) => {
         if (this._sourceObj && this._sourceObj.url) {
           this._trigger(EventType.ABR_MODE_CHANGED, {mode: this.isAdaptiveBitrateEnabled() ? 'auto' : 'manual'});
-          const shakaStartTime = startTime && startTime > -1 ? startTime : undefined;
+          let shakaStartTime = startTime && startTime > -1 ? startTime : undefined;
+          shakaStartTime = this._lastTimeDetach || shakaStartTime;
           this._maybeGetRedirectedUrl(this._sourceObj.url)
             .then(url => {
               return this._shaka.load(url, shakaStartTime);
