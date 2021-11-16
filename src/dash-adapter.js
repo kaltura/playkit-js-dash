065e817124de21e484f87e4fb8fb1080cb83c4b0
yairansbacher <yairans@gmail.com>
2021-11-16 11:10:46 +0200

    fix file name typo

--- src/dash-adapter.js
+++ src/dash-adapter.js
@@ -18,7 +18,7 @@ import {
 import {Widevine} from './drm/widevine';
 import {PlayReady} from './drm/playready';
 import DefaultConfig from './default-config';
-import './assets/syle.css';
+import './assets/style.css';
 import {DashManifestParser} from './parser/dash-manifest-parser';
 import {DashThumbnailController} from './dash-thumbnail-controller';
 
