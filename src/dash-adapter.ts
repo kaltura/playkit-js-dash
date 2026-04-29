488fdac19b792afb57a67d7f1687b170ea394dc1
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2026-04-29 16:18:23 +0300

    Update dash-adapter.ts

--- src/dash-adapter.ts
+++ src/dash-adapter.ts
@@ -1025,18 +1025,18 @@ export default class DashAdapter extends BaseMediaSourceAdapter {
    * @private
    */
   private _getAudioTracks(): ShakaAudioTrack[] {
-    const variantTracks = this._shaka.getVariantTracks();
-    const audioTracks = this._shaka.getAudioLanguagesAndRoles();
-    audioTracks.forEach(track => {
-      const sameLangAudioVariants = variantTracks.filter(vt => vt.language === track.language && (!track.role || !vt.audioRoles || vt.audioRoles.includes(track.role)));
-      const id = sameLangAudioVariants.map(variant => variant.id).join('_');
-      const active = sameLangAudioVariants.some(variant => variant.active);
-      const isAccessible = sameLangAudioVariants.some(variant => variant.accessibilityPurpose);
-      track['id'] = id;
-      track.label = sameLangAudioVariants[0].label;
-      track['active'] = active;
-      track['kind'] =  isAccessible ? AudioTrackKind.DESCRIPTION : AudioTrackKind.MAIN;
-    });
+    //const variantTracks = this._shaka.getVariantTracks();
+    const audioTracks = this._shaka.getAudioTracks();//getAudioLanguagesAndRoles();
+    // audioTracks.forEach(track => {
+    //   const sameLangAudioVariants = variantTracks.filter(vt => vt.language === track.language && (!track.role || !vt.audioRoles || vt.audioRoles.includes(track.role)));
+    //   const id = sameLangAudioVariants.map(variant => variant.id).join('_');
+    //   const active = sameLangAudioVariants.some(variant => variant.active);
+    //   const isAccessible = sameLangAudioVariants.some(variant => variant.accessibilityPurpose);
+    //   track['id'] = id;
+    //   track.label = sameLangAudioVariants[0].label;
+    //   track['active'] = active;
+    //   track['kind'] =  isAccessible ? AudioTrackKind.DESCRIPTION : AudioTrackKind.MAIN;
+    // });
     return audioTracks as ShakaAudioTrack[];
   }
 
