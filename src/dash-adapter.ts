3698858105e3b3ffdce021ace0f98b27e9d33a15
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2026-05-05 11:10:21 +0300

    Update dash-adapter.ts

--- src/dash-adapter.ts
+++ src/dash-adapter.ts
@@ -1183,7 +1183,7 @@ export default class DashAdapter extends BaseMediaSourceAdapter {
    */
   public selectAudioTrack(audioTrack: AudioTrack): void {
     if (this._shaka && audioTrack instanceof AudioTrack && !audioTrack.active) {
-      this._shaka.selectAudioLanguage(audioTrack.language);
+      this._shaka.selectAudioTrack(audioTrack as unknown as any);
       this._onTrackChanged(audioTrack);
     }
   }
